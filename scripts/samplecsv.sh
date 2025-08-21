e4cf3554713a9b422478b05c1d196ea6c23a397c
shourov.foisal <shourov.foisal@technonext.com>
2025-08-21 16:32:46 +0600

    Fix csv extracted sample file name

--- scripts/samplecsv.sh
+++ scripts/samplecsv.sh
@@ -1,2 +1,2 @@
 csv_file=$(ls *.csv | head -n 1) && \
-(head -n 1 "$csv_file" && tail -n +2 "$csv_file" | head -n 10000) > output.csv
+(head -n 1 "$csv_file" && tail -n +2 "$csv_file" | head -n 10000) > sample.csv
